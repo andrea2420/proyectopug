a9d90dd912c1d3ab2973f9618d6b711c2ec316aa
andrea <gabisago@hotmail.com>
2024-09-13 01:22:24 -0300

    segundo commit

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,46 @@
-import express from 'express';
+import express, {Request, Response }  from "express";
+import cors from 'cors';
+import morgan from "morgan";
+import path from "path";
+import estudianteRouter from'./routes/estudianteRouter';
+import profesorRouter from './routes/profesorRoutes';
+import inscripcionRouter from './routes/inscripcionRouter';
+import cursoRouter from './routes/cursosRouter';
+//import methodOverride from 'method-override';
+
+const app=express();
+
+//habilitamos pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/views'));
+//carpeta pblica
+app.use(express.static('public'));
+
+
+
+//app.use(methodOverride('_method'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/',(req:Request,res:Response)=>{
+    console.log(__dirname);
+    return res.render('index', {
+        pagina: 'App Univerdsidad',
+       // errores: errores.array()
+    });
+});
+app.use('/estudiantes', estudianteRouter);
+app.use('/profesores',profesorRouter );
+app.use('/cursos',cursoRouter );
+app.use('/inscripciones',inscripcionRouter);
+
+export default app;
+
+
+/*import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 const port =3000;
@@ -20,4 +62,4 @@ app.use('/profesores',profesorRouter );
 app.use('/cursos',cursoRouter );
 app.use('/inscripciones',inscripcionRouter);
 
-export default app;
+export default app;*/
